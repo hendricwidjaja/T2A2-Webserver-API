d34f5978c1d290c710b042bd0d885eed1ce02afc
hendricwidjaja <hcwidjaja@gmail.com>
2024-09-28 03:28:22 +1000

    Draft README file. Auth controller: Add error handling for when user cannot be found for delete route. User model: update error message for invalid username

---
+++ README.md
@@ -0,0 +1,17 @@
+# T2A2 API Webserver - Workout Together Planner API
+
+Welcome to the documentation of Hendric Widjaja's - T2A2: API Webserver.
+
+# Quick Links
+
+Please use the links below to quickly access key parts of this documentation
+
+- [Link to source control repository (GitHub)](https://github.com/hendricwidjaja/T2A2-Webserver-API)
+- [R1 - Explain the problem that this app will solve and how this app addresses the problem]
+- [R2 - Task allocation & Tracking]
+- [R3 - Third-Party Services, Packages & Dependencies]
+- [R4 - Benefits & Drawbacks of Postgresql]
+- [R5 - Explain the features, purpose & functionalities of the object-relational mapping system (ORM) used in this app]
+- [R6 - ERD]
+- [R7 - Explain the implemeneted models and their relationships, including how the relationships aid database implementation]
+- [R8 - API Endpoints Tutorial & Explanation]
--- models/user.py
+++ models/user.py
@@ -21,7 +21,7 @@ class User(db.Model):
     likes = db.relationship("Like", back_populates="user", cascade="all, delete")
 
 class UserSchema(ma.Schema):
-    username = fields.String(required=True, validate=Regexp(r"^(?=.{4,20}$)(?!.*[_.]{2})[a-zA-Z0-9._]+$", error="Username must be 4-20 characters long. It can only contain letters, digits, periods(.) and or underscores(_). Consecutive periods or underscores are not permitted."))
+    username = fields.String(required=True, validate=Regexp(r"^(?=.{4,20}$)(?!.*[_.]{2})[a-zA-Z0-9._]+$", error="Username must be 4-20 characters long (no spaces). It can only contain letters, digits, periods(.) and or underscores(_). Consecutive periods or underscores are not permitted."))
     email = fields.String(required=True, validate=Regexp(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", error="Invalid Email Format"))
     password = fields.String(required=True, validate=Regexp(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,20}$", error="Password must be 6-20 characters long and must include at least one letter (a-z, A-Z), one number (0-9) & one special character (@$!%*#?&)"))
     firstname = fields.String(validate=Length(max=50), error="Cannot exceed 50 characters")
